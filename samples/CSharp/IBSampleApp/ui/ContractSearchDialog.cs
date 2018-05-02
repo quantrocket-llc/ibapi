f4a4f5a488668b3d68438a47e15e324f1d9f24dd
Michael Vorobyev <mvorobyev@interactivebrokers.ru>
2018-05-02 22:34:32 +0300

    Update license header

--- samples/CSharp/IBSampleApp/ui/ContractSearchDialog.cs
+++ samples/CSharp/IBSampleApp/ui/ContractSearchDialog.cs
@@ -1,4 +1,7 @@
-﻿using IBApi;
+﻿/* Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+using IBApi;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
--- samples/VB/VB_API_Sample/dlgSmartComponents.vb
+++ samples/VB/VB_API_Sample/dlgSmartComponents.vb
@@ -1,4 +1,7 @@
-﻿Imports System.Windows.Forms
+﻿' Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+' and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
+
+Imports System.Windows.Forms
 
 Public Class dlgSmartComponents
 
