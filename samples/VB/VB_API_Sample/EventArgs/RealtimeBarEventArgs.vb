e9250b2365e53b5d29e43c7f44807f8a07513e88
Michael Vorobyev <mvorobyev@interactivebrokers.ru>
2019-01-10 20:48:47 +0300

    update license header

--- samples/VB/VB_API_Sample/EventArgs/RealtimeBarEventArgs.vb
+++ samples/VB/VB_API_Sample/EventArgs/RealtimeBarEventArgs.vb
@@ -1,5 +1,5 @@
 ﻿Imports IBApi
-' Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+' Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
 ' and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
 
 Class RealtimeBarEventArgs
